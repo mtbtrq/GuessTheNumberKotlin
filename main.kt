68f0d31d2966db059649a3c5458e6fdc40f4e073
Supelion <mutayyabtariq9@gmail.com>
2021-11-07 21:54:42 +0300

    It's pretty now 😍

--- main.kt
+++ main.kt
@@ -1,16 +1,25 @@
 fun main() {
-    var guessedCorrect: Boolean = false
+    var guessedCorrect: Boolean = false;
 
     while (!guessedCorrect) {
-        println("Enter a guess:")
-        val guess = readLine().toString()
-        val randomNumber = ((Math.floor(Math.random() * 10)).toInt()).toString();
+        println("\n\nEnter a guess:");
+        val guess: Int = readLine()!!.toInt();
+        val newGuess: String;
 
-        if (guess == randomNumber) {
-            guessedCorrect = true
-            println("You guessed the correct number!")
+        if (guess > 10) {
+            println("\n\nPlease only guess a number between 1 and 10!")
+            continue;
+        }
+        
+        newGuess = guess.toString();
+
+        val randomNumber = (Math.round(Math.random() * 10)).toString();
+
+        if (newGuess == randomNumber) {
+            guessedCorrect = true;
+            println("\n\nYou guessed the correct number!");
         } else {
-            println("Incorrect guess, the correct number was: $randomNumber")
+            println("\n\nIncorrect guess, the correct number was: $randomNumber");
         }
     }
 }
